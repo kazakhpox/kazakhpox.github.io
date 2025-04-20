400cf611b4ddbc039cb0c74f020b3b53653c04a5
kazakhpox <saginbaev.darkhan@mail.ru>
2025-04-20 16:31:19 +0500

    2123

--- html_courses/html_course_script.js
+++ html_courses/html_course_script.js
@@ -2,3 +2,6 @@ function runCode() {
     const code = document.getElementById("code").value;
     document.getElementById("output").innerHTML = code;
   }  
+document.querySelectorAll('code').forEach((block) => {
+    block.innerHTML = block.innerHTML.replace(/(&lt;[^&]+&gt;)/g, '<span class="green">$1</span>');
+  });
