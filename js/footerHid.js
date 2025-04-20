71e54c5cf36b7f4f770b9a7d1d59ce8b77d92001
kazakhpox <saginbaev.darkhan@mail.ru>
2025-04-20 21:40:05 +0500

    р

--- js/footerHid.js
+++ js/footerHid.js
@@ -3,9 +3,13 @@ document.addEventListener("scroll", () => {
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollPosition = window.innerHeight + window.scrollY;
 
+    console.log("Scroll position:", scrollPosition, "Scroll height:", scrollHeight);
+
     if (scrollPosition >= scrollHeight - 10) { 
         footer.classList.add("show"); // Показываем футер
+        console.log("Footer shown");
     } else {
         footer.classList.remove("show"); // Скрываем футер
+        console.log("Footer hidden");
     }
 });
